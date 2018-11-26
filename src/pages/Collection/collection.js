0d99b56754648d01dedf7c9abbda12b42a32d716
Adam Meszaros <adam.meszaros@digirati.com>
2018-11-26 13:47:52 +0000

    fixed warning in collections

--- src/pages/Collection/collection.js
+++ src/pages/Collection/collection.js
@@ -25,12 +25,12 @@ class CollectionPage extends React.Component {
           { renderSlideShow }
         </div>
         <div className="article-main">
-          <h1>{pageContext && pageContext.metadata && pageContext.metadata.filter(item=>item.label=='Title').map(item=>item.value).join(' ')}</h1>
+          <h1>{pageContext && pageContext.metadata && pageContext.metadata.filter(item=>item.label==='Title').map(item=>item.value).join(' ')}</h1>
           {
             pageContext && 
             pageContext.metadata && 
             pageContext.metadata.filter(
-              item=>item.label=='Description'
+              item=>item.label==='Description'
             ).map(
               item=>item.value
             )
