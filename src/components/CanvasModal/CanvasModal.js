139b2455d49ef4c443b16f5fc1a0b48713d26b37
Adam Meszaros <adam.meszaros@digirati.com>
2018-12-14 07:12:10 +0000

    updated the media modals

--- src/components/CanvasModal/CanvasModal.js
+++ src/components/CanvasModal/CanvasModal.js
@@ -12,23 +12,61 @@ import {
 } from '@canvas-panel/slideshow';
 
 import './CanvasModal.scss';
+import { getTranslation } from '../../utils';
 
-const CanvasModal = ({ canvas, hideCanvasDetails }) => (
+const CanvasModal = ({ canvas, hideCanvasDetails }) => {
+
+
+  const processedCanvases = [];
+  if (canvas) {
+    const mediaTypes = ['Image', 'Video'];
+    let lastMediaCanvas = null;
+    canvas.items[0].items.forEach((annotation, index) => {
+      
+      if (mediaTypes.indexOf(annotation.body.type) !== -1) {
+        if (lastMediaCanvas !== null) {
+          processedCanvases.push(lastMediaCanvas);
+        }
+        const copyAnnotation = JSON.parse(JSON.stringify(annotation));
+        copyAnnotation.target = `canvas-${index}`;
+        lastMediaCanvas = {
+          id: `canvas-${index}`,
+          type: 'Canvas',
+          width: canvas.width || annotation.width,
+          height: canvas.height || annotation.height,
+          items: [{
+            id: `annotation-page-${index}`,
+            type: 'AnnotationPage',
+            items: [copyAnnotation],
+          }]
+        }
+      }
+    });
+
+    if (lastMediaCanvas !== null) {
+      lastMediaCanvas.annotations = canvas.annotations;
+      processedCanvases.push(lastMediaCanvas);
+    }
+  }
+
+  const pseudoManifest = {
+    "@context": [
+      "http://www.w3.org/ns/anno.jsonld",
+      "http://iiif.io/api/presentation/3/context.json"
+    ],
+    "type": "Manifest",
+    "id": "http://digirati.com/iiif/v3/temporary/manifest",
+    items: processedCanvases,
+  };
+
+  console.log(pseudoManifest);
+
+  return (
     canvas 
       ? (
         <div className="canvas-modal">
           <div className="canvas-modal__content">
-            <Manifest jsonLd={{
-              "@context": [
-                "http://www.w3.org/ns/anno.jsonld",
-                "http://iiif.io/api/presentation/3/context.json"
-              ],
-              "type": "Manifest",
-              "id": "http://digirati.com/iiif/v3/temporary/manifest",
-              items: [
-                canvas,
-              ],
-            }}>
+            <Manifest jsonLd={pseudoManifest}>
               <RangeNavigationProvider>
                 {rangeProps => {
                   const {
@@ -54,30 +92,94 @@ const CanvasModal = ({ canvas, hideCanvasDetails }) => (
                       ) : (
                         <>
                         <div className={'canvas-modal__content-slide'}>
+                          <div className={'canvas-modal__slide-transition-wrapper'}>
                           <SimpleSlideTransition id={currentIndex}>
-                            <Slide
-                              behaviors={canvas.__jsonld.behavior || []}
-                              manifest={manifest}
-                              canvas={canvas}
-                              region={region}
-                              fullscreenProps={{
-                                isFullscreen: false,
-                              }}
-                            />
+                            {canvas.__jsonld && 
+                            canvas.__jsonld.items && 
+                            canvas.__jsonld.items.length > 0 &&
+                            canvas.__jsonld.items[0].items &&
+                            canvas.__jsonld.items[0].items.length &&
+                            canvas.__jsonld.items[0].items[0] && 
+                            canvas.__jsonld.items[0].items[0].body &&
+                            canvas.__jsonld.items[0].items[0].body.type === 'Video' ? (
+                              <div
+                                style={{
+                                  width: '100%',
+                                  height: '100%',
+                                  display: 'flex',
+                                  justifyContent: 'center',
+                                  alignItems: 'center',
+                                  background: 'rgb(55, 55, 55)',
+                                }}
+                              >
+                                <video 
+                                  src={canvas.__jsonld.items[0].items[0].body.id} 
+                                  width={canvas.width} 
+                                  height={canvas.height}
+                                  controls
+                                  autoplay
+                                />
+                              </div>
+                            ): (
+                              <Slide
+                                behaviors={canvas.__jsonld.behavior || []}
+                                manifest={manifest}
+                                canvas={canvas}
+                                region={region}
+                                fullscreenProps={{
+                                  isFullscreen: false,
+                                }}
+                              />
+                            )}
                           </SimpleSlideTransition>
+                          </div>
                           </div>
                           <div className={'canvas-modal__info-and-nav'}>
                             <div className={'canvas-modal__info'}>
-                              
+                            {canvas.__jsonld && 
+                            canvas.__jsonld.items && 
+                            canvas.__jsonld.items.length > 0 &&
+                            canvas.__jsonld.items[0].items &&
+                            canvas.__jsonld.items[0].items.length &&
+                            canvas.__jsonld.items[0].items[0].label ? (
+                              <h6>
+                                {getTranslation(canvas.__jsonld.items[0].items[0].label, 'en')}
+                              </h6>
+                            ) : ''}
+                            {canvas.__jsonld && 
+                            canvas.__jsonld.items && 
+                            canvas.__jsonld.items.length > 0 &&
+                            canvas.__jsonld.items[0].items &&
+                            canvas.__jsonld.items[0].items.length &&
+                            canvas.__jsonld.items[0].items[0].summary ? (
+                              <p>
+                                {getTranslation(canvas.__jsonld.items[0].items[0].summary, 'en')}
+                              </p>
+                            ) : ''}
                             </div>
-                            <div className={'canvas-modal__nav'}>
-                              <CanvasNavigation
-                                previousRange={previousRange}
-                                nextRange={nextRange}
-                                canvasList={canvasList}
-                                currentIndex={currentIndex}
-                              />
-                            </div>
+                            {canvasList.length > 1 ? (
+                              <div className={'canvas-modal__nav'}>
+                                {currentIndex + 1} / {canvasList.length}
+                              </div>
+                            ) : ''}
+                            {canvasList.length > 1 ? (
+                              <div className={'canvas-modal__nav'}>
+                                {previousRange && (
+                                  <button className="arrow left" onClick={previousRange}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                      <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+                                    </svg>
+                                  </button>
+                                )}
+                                {nextRange && (
+                                  <button className="arrow right" onClick={nextRange}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                      <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+                                    </svg>
+                                  </button>
+                                )}
+                              </div>
+                            ) : ''}
                           </div>
                         </>
                       )}
@@ -119,5 +221,6 @@ const CanvasModal = ({ canvas, hideCanvasDetails }) => (
       )
       : ''
   );
+}
 
-  export default CanvasModal;
+export default CanvasModal;
