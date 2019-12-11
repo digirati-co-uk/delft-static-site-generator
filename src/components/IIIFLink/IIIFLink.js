5da16c09e68d0a9f1b36dc64c802dd2048da3c09
Kelsie <kelsie.braidwood@digirati.com>
2019-12-11 13:51:31 +0000

    Styles the link and adds title attribute

--- src/components/IIIFLink/IIIFLink.js
+++ src/components/IIIFLink/IIIFLink.js
@@ -3,22 +3,20 @@ import PropTypes from 'prop-types';
 import IIIFIcon from '../../../public/icons/logo-iiif-34x30.png';
 import './IIIFLink.scss';
 
-const GITHUB_BASE = 'https://github.com/digirati-co-uk/delft-static-site-generator/blob/master/';
 const RAW_BASE = 'https://raw.githubusercontent.com/digirati-co-uk/delft-static-site-generator/master/src';
 
 const getIIIFLink = (href) => {
   if (/\/(collections|objects|exhibitions)\//.test(href)) {
-    const baselink = `${href.replace(/(en|nl)/, `${GITHUB_BASE}src`)}.json`;
+    const baselink = `${href.replace(/(en|nl)/, `${RAW_BASE}`)}.json`;
     const manifest = `${href.replace(/(en|nl)/, `?manifest=${RAW_BASE}`)}.json`;
     return baselink + manifest;
   }
-  return `${GITHUB_BASE}content/${href}.md`;
+  return `${RAW_BASE}content/${href}.md`;
 };
 
 const IIIFLink = ({ href }) => (
-  <a className="iiif-link-wrapper" href={getIIIFLink(href)}>
+  <a className="iiif-link-wrapper" href={getIIIFLink(href)} title="Drag and Drop IIIF Resource">
     <img src={IIIFIcon} alt="IIIF logo" />
-    IIIF Manifest
   </a>
 );
 
