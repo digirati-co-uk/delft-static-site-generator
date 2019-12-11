4af8104adf5e3af2f6e87daca7429e9d77c36b50
Kelsie <kelsie.braidwood@digirati.com>
2019-12-11 13:51:08 +0000

    Sets link to raw github json

--- src/components/IIIFLink/IIIFLink.js
+++ src/components/IIIFLink/IIIFLink.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import IIIFIcon from '../../../public/icons/logo-iiif-34x30.png';
 import './IIIFLink.scss';
 
-const GITHUB_BASE = 'https://github.com/digirati-co-uk/delft-static-site-generator/blob/master/src';
-const RAW_BASE = 'https://raw.githubusercontent.com/digirati-co-uk/delft-static-site-generator/master/src/';
+const GITHUB_BASE = 'https://github.com/digirati-co-uk/delft-static-site-generator/blob/master/';
+const RAW_BASE = 'https://raw.githubusercontent.com/digirati-co-uk/delft-static-site-generator/master/src';
 
 const getIIIFLink = (href) => {
   if (/\/(collections|objects|exhibitions)\//.test(href)) {
-    return `${href.replace(/(en|nl)/, `dummy?manifest=${RAW_BASE}`)}.json`;
+    const baselink = `${href.replace(/(en|nl)/, `${GITHUB_BASE}src`)}.json`;
+    const manifest = `${href.replace(/(en|nl)/, `?manifest=${RAW_BASE}`)}.json`;
+    return baselink + manifest;
   }
   return `${GITHUB_BASE}content/${href}.md`;
 };
