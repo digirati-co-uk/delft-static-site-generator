c92a6f3b89818aa2144d8c0243e7527a189e6120
Kelsie Braidwood <33040231+ionab@users.noreply.github.com>
2019-12-16 13:32:40 +0000

    Del 38/adding articles (#19)
    * Adds demo article to the nl homepage
    
    * Adds padding for consistency with styles
    
    * styling change
    
    * Data coming through
    
    * returning author and date for articles on homepage
    
    * removed console log
    
    * remove unused variables
    
    * remove comment

--- src/utils.js
+++ src/utils.js
@@ -3,30 +3,45 @@
  * @param {String} html - html string to post process
  * @returns {String} the post processed html
  */
-const substituteSpecialLinks = (html, pageContext) => html.replace(
-    /(<p><a href="(?:\/(en|nl))(\/(collection|exhibition|object)s\/.*)">)([^<]+)<\/a><\/p>/g,
+const substituteSpecialLinks = (html, pageContext, allMDRemark) => html.replace(
+    /(<p><a href="(?:\/(en|nl))(\/(collection|exhibition|object|publication)s\/.*)">)([^<]+)<\/a><\/p>/g,
     (match, p1, p2, p3, p4, p5) => {
       const hasThumbnail = p3 && pageContext && pageContext.thumbnails
         && pageContext.thumbnails.hasOwnProperty(p3.substr(1));
       if (hasThumbnail) {
-        const template = `
-          <a href="/${p2}${p3}" class="cover-link">
+        return (
+          `<a href="/${p2}${p3}" class="cover-link">
             <div class="image">
               <img class="bg" src="${pageContext.thumbnails[p3.substr(1)]}">
             </div>
-            <div class="content">
-              <div class="boxtitle">${p4}</div>
-              <div class="maintitle">${p5}</div>
-              <div></div>
+              <div class="content">
+                <div class="boxtitle">${p4}</div>
+                <div class="maintitle">${p5}</div>
+                <div></div>
+              </div>
+          </a>`
+          );
+        } if (p4 === 'publication') {
+          return `<a href="/${p2}${p3}">
+            <div class="boxtitle">ARTICLE
             </div>
-
-          </a>
-        `;
-        return template;
+            <div class="maintitle">${p5}</div>
+            <div>${getAuthor(`/${p2}${p3}`, allMDRemark)}</div>
+          </a>`;
       }
-        return `<a href="/${p2}${p3}"><div class="boxtitle">${p4}</div><div class="maintitle">${p5}</div><div></div></a>`;
     },
   );
+
+const getAuthor = (path, allMDRemark) => {
+  let author; let date;
+  allMDRemark.edges.forEach((markdown) => {
+    if (markdown.node.frontmatter.path === path) {
+      author = markdown.node.frontmatter.author;
+      date = markdown.node.frontmatter.date;
+    }
+  });
+  return `${author} ${date}`;
+};
 
 export const getTranslation = (obj, lang, glue = ' ') => (obj ? obj[lang] || obj['@none'] || obj.none || [] : []).join(glue);
 
