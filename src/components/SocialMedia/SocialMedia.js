967fba28cff26fc92bf80f5e48fcf5f294ecf836
Kelsie <kelsie.braidwood@digirati.com>
2019-12-18 13:33:57 +0000

    Facebook working

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TwitterShareButton, TwitterIcon } from 'react-share';
+import {
+ TwitterShareButton, TwitterIcon, FacebookIcon, FacebookShareButton, LinkedinShareIcon, LinkedinShareButton, WhatsappShareButton, WhatsappShareIcon, RedditShareButton,
+} from 'react-share';
 import './SocialMedia.scss';
 
 
-export const SocialMedia = ({ socialConfig, tags }) => (
-  <React.Fragment>
-    <TwitterShareButton
-      url={socialConfig.config.url}
-      title={socialConfig.config.title}
-      via={socialConfig.twitterHandle.split('@').join('')}
-      hashtags={tags}
-    >
-      <TwitterIcon />
-      <span className="text">Twitter</span>
-    </TwitterShareButton>
-  </React.Fragment>
-);
+export const SocialMedia = ({ socialConfig, tags }) => <React.Fragment>
+           <FacebookShareButton url={socialConfig.config.url} className="button is-outlined is-rounded facebook">
+             <FacebookIcon />
+           </FacebookShareButton>
+           <TwitterShareButton url={socialConfig.config.url} title={socialConfig.config.title} via={socialConfig.twitterHandle
+               .split('@')
+               .join(
+                 ''
+               )} hashtags={tags} className="social" title="Share on Twitter">
+             <TwitterIcon />
+           </TwitterShareButton>
+           {/* <LinkedinShareButton url={socialConfig.config.url} className="button is-outlined is-rounded linkedin" title={socialConfig.config.title}>
+             <LinkedinShareIcon />
+            </LinkedinShareButton>
+           <RedditShareButton url={socialConfig.config.url} className="button is-outlined is-rounded reddit" title={socialConfig.config.title} />
+           <WhatsappShareButton url={socialConfig.config.url} className="button is-outlined is-rounded whatsapp" title={socialConfig.config.title} /> */}
+         </React.Fragment>;
 
 SocialMedia.propTypes = {
   socialConfig: PropTypes.shape({
