f4a0ebf8277dd239c6294e5f5e25f45d95fa7017
Kelsie <kelsie.braidwood@digirati.com>
2019-12-19 12:52:56 +0000

    Language selector only works if the language page is there

--- src/components/LanguageSelector/languageSelector.js
+++ src/components/LanguageSelector/languageSelector.js
@@ -1,20 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 import './languageSelector.css';
 
-const LanguageSelector = ({ path = '' }) => {
+const LanguageSelector = ({ data, path = '' }) => {
   const pathNoLocale = path.replace(/^(en|nl)/, '');
+  const englishPath = `/en${pathNoLocale}`;
+  const dutchPath = `/nl${pathNoLocale}`;
+  let allowEnglish = false;
+  let allowDutch = false;
+
+  data.allSitePage.edges.forEach((site) => {
+    if (site.node.path === englishPath) {
+      console.log(site);
+      allowEnglish = true;
+    }
+    if (site.node.path === dutchPath) {
+      console.log(site);
+      allowDutch = true;
+    }
+  });
+
+
   return (
     <span className="language-selector">
-      <a href={`/en${pathNoLocale}`} className="language-selector__translation">EN</a>
+      <a href={allowEnglish ? englishPath : null} className="language-selector__translation" disabled={!allowEnglish}>
+        EN
+      </a>
       /
-      <a href={`/nl${pathNoLocale}`} className="language-selector__translation">NL</a>
+      <a href={allowDutch ? dutchPath : null} className="language-selector__translation">
+        NL
+      </a>
     </span>
   );
 };
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      {
+        allSitePage {
+          edges {
+            node {
+              id
+              path
+            }
+          }
+        }
+      }
+    `}
+    render={data => <LanguageSelector data={data} {...props} />}
+  />
+);
 
 LanguageSelector.propTypes = {
   path: PropTypes.string.isRequired,
 };
 
-export default LanguageSelector;
+// export default LanguageSelector;
