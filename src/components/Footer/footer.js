c805675612c2830e04c0766d716681ef7866b071
Kelsie Braidwood <33040231+ionab@users.noreply.github.com>
2020-01-07 10:50:25 +0000

    fixes build errors (#34)

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,17 +3,22 @@ import PropTypes from 'prop-types';
 import { SocialMedia } from '../SocialMedia/SocialMedia';
 
 import './footer.css';
-const Footer = ({ title, twitterHandle }) => (
-  <footer className="footer">
-    <SocialMedia
-      socialConfig={{
-        twitterHandle,
-        config: { ...{ url: location }, title },
-      }}
-      tags={[]}
-    />
-  </footer>
-);
+
+const Footer = ({ path, title, twitterHandle, url }) => {
+  const currentPath =
+    typeof window !== `undefined` ? location : `${url}${path}`;
+  return (
+    <footer className="footer">
+      <SocialMedia
+        socialConfig={{
+          twitterHandle,
+          config: { ...{ url: currentPath }, title },
+        }}
+        tags={[]}
+      />
+    </footer>
+  );
+};
 
 Footer.propTypes = {
   path: PropTypes.string.isRequired,
