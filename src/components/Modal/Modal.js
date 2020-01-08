eae49ac0f9dd28adb888702e1660da9516f1cc7f
Kelsie <kelsie.braidwood@digirati.com>
2020-01-08 09:34:08 +0000

    Adds styling to basic modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Close } from '../Close/Close';
+
+import './Modal.scss';
 
 export const Modal = ({summary, close}) => {
   console.log(summary)
-  return (
-    <div>
-      {summary}
-      <button onClick={close}>Close</button>
-    </div>
-  )
+  return <div className="modal">
+      <div className="modal__details">{summary}</div>
+      <button onClick={close} className="modal__close" type="button">
+        <Close />
+      </button>
+    </div>;
 }
 
 Modal.propTypes = {
