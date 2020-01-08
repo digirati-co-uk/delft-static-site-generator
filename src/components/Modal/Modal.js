90bb9cbeb6607f277833cc71cde3a9b22cd25cb6
Kelsie Braidwood <33040231+ionab@users.noreply.github.com>
2020-01-08 10:39:31 +0000

    Del 44/metadata (#39)
    * Allows html in the metadata of json
    
    * Revert changes to the html
    
    * Adds example html to the test.json file
    
    * refactors collection page to class component
    
    * Adds basic modal
    
    * Adds styling to basic modal
    
    * allows modal to render html
    
    * removes the old functional collection page component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Close } from '../Close/Close';
+
+import './Modal.scss';
+
+export const Modal = ({modalContent, close}) => {
+  return <div className="modal">
+      <div className="modal__details">{modalContent}</div>
+      <button onClick={close} className="modal__close" type="button">
+        <Close />
+      </button>
+    </div>;
+}
+
+Modal.propTypes = {
+  modalContent: PropTypes.array,
+  close: PropTypes.func
+}
