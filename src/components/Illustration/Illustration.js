e2df4979cd12887e22b44ac60172a77f3036cd39
Kelsie <kelsie.braidwood@digirati.com>
2020-04-06 20:11:35 +0100

    Single canvas link generation working

--- src/components/Illustration/Illustration.js
+++ src/components/Illustration/Illustration.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { PropTypes } from 'prop-types';
 
 import DynamicCanvasModal from '../CanvasModal/DynamicCanvasModal';
+import { StaticQuery, graphql } from 'gatsby';
 
 const fetchDataFromFile = source => {
   const json = require(`../../../content/illustrations/${source}`);
@@ -26,31 +27,55 @@ const getThumbnail = manifest => {
   return thumbnail;
 };
 
+const getGraphThumbnail = node => {
+  if (!node.items) return [];
+  let thumbnails = node && node.items ? node.items : [];
+  thumbnails = thumbnails.map(item => {
+    if (item && item.thumbnail && item.thumbnail[0] && item.thumbnail[0].id)
+      return item.thumbnail[0].id;
+  });
+  return thumbnails;
+};
+
 const fixUrl = url => {
   return url
     .replace('thumbs', 'iiif-img')
     .replace('/full/full/0/default.jpg', '');
 };
 
-export const Illustration = ({ source, pageLanguage, object, children }) => {
+export const Illustration = ({ source, pageLanguage, children }) => {
   const [iiifmanifest, setIiifManifest] = useState({});
   const [thumbnailSrc, setThumbnailSrc] = useState('');
   const [showCanvasModal, setCanvasModal] = useState(false);
   const [annos, setAnnos] = useState([]);
+  const [paths, setPaths] = useState([]);
+  const [link, setLink] = useState('');
   useEffect(() => {
     setIiifManifest(fetchDataFromFile(source));
   }, []);
-
-  const link = object ? `objects/${object}` : '';
 
   useEffect(() => {
     const thumbnail = getThumbnail(iiifmanifest);
     const key = thumbnail.id ? fixUrl(thumbnail.id) : '';
     if (thumbnail.id) setThumbnailSrc(thumbnail.id);
     if (thumbnail.id) setAnnos({ [key]: link });
-  }, [iiifmanifest]);
+  }, [iiifmanifest, link]);
 
-  const renderModal = () => {
+  const renderModal = data => {
+    const obj = data.allSitePage.nodes.filter(node => {
+      const thumbnails = getGraphThumbnail(node.context);
+      return thumbnails.includes(thumbnailSrc);
+    });
+    setPaths(obj);
+    let _link = '';
+    if (paths.length > 1) {
+      _link = paths.find(route =>
+        route.path.split('/')[1].includes(pageLanguage)
+      ).path;
+    }
+    _link = _link.replace('/en/', '');
+    _link = _link.replace('/nl/', '');
+    setLink(_link);
     setCanvasModal(!showCanvasModal);
   };
 
@@ -58,31 +83,52 @@ export const Illustration = ({ source, pageLanguage, object, children }) => {
     iiifmanifest && iiifmanifest.items && iiifmanifest.items[0]
       ? iiifmanifest.items[0]
       : {};
+
   return (
-    <>
-      <div className="cutcorners w-7 h-4">
-        <img
-          style={{
-            objectFit: 'cover',
-            cursor: 'pointer',
-            width: '100%',
-            height: '100%',
-          }}
-          src={thumbnailSrc}
-          onClick={() => renderModal()}
-        ></img>
-      </div>
-      {children ? <div className="info cutcorners">{children}</div> : <></>}
-      {showCanvasModal ? (
-        <DynamicCanvasModal
-          manifest={iiifmanifest}
-          selectedCanvas={canvas}
-          annotationDetails={annos}
-          hideCanvasDetails={() => setCanvasModal(false)}
-          pageLanguage={pageLanguage}
-        ></DynamicCanvasModal>
-      ) : null}
-    </>
+    <StaticQuery
+      query={graphql`
+        query MyQuery {
+          allSitePage {
+            nodes {
+              path
+              context {
+                items {
+                  thumbnail {
+                    id
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <>
+          <div className="cutcorners w-7 h-4">
+            <img
+              style={{
+                objectFit: 'cover',
+                cursor: 'pointer',
+                width: '100%',
+                height: '100%',
+              }}
+              src={thumbnailSrc}
+              onClick={() => renderModal(data)}
+            ></img>
+          </div>
+          {children ? <div className="info cutcorners">{children}</div> : <></>}
+          {showCanvasModal ? (
+            <DynamicCanvasModal
+              manifest={iiifmanifest}
+              selectedCanvas={canvas}
+              annotationDetails={annos}
+              hideCanvasDetails={() => setCanvasModal(false)}
+              pageLanguage={pageLanguage}
+            ></DynamicCanvasModal>
+          ) : null}
+        </>
+      )}
+    />
   );
 };
 
