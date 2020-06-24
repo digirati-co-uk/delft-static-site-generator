960393a8377a10a36ca94b62853fffae9320840e
Kelsie <kelsie.braidwood@digirati.com>
2020-06-24 21:40:28 +0100

    Improve quality of the youtube thumbnail placeholder

--- src/components/AnnotationBodyRenderer/AnnotationBodyRenderer.js
+++ src/components/AnnotationBodyRenderer/AnnotationBodyRenderer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const filterToPreferredChoices = (choices, pageLanguage = 'en') =>
@@ -102,34 +102,39 @@ IIIFImageAnnotationCover.defaultProps = {
 };
 
 const IIIFVideoAnnotationCover = ({ body, position }) => {
-  const [imageUrl, setImageUrl] = useState(body.id);
+  const [imageUrl, setImageUrl] = useState('');
   let url = body.id;
-  if (body.id.includes('youtu.be') || body.id.includes('youtube')) {
-    url = url.replace('watch', 'embed');
-    url = url.replace('youtube', 'youtube.com/embed');
-    url = url.replace('youtu.be', 'youtube.com/embed');
-    url =
-      url +
-      `?start=${body.selector.value.split('t=')[1].split(',')[0]}&end=${
-        body.selector.value.split('t=')[1].split(',')[1]
-      }`;
-    setImageUrl(
-      `https://img.youtube.com/vi/${url.split('/')[4].split('?')[0]}/0.jpg`
-    );
-  }
-  if (body.id.includes('vimeo')) {
-    url = url.replace('vimeo.com', 'player.vimeo.com/video');
-    if (body.selector && body.selector.value.includes('t=')) {
-      url = url + `#${body.selector.value.split(',')[0]}`;
+
+  useEffect(() => {
+    if (body.id.includes('youtu.be') || body.id.includes('youtube')) {
+      url = url.replace('watch', 'embed');
+      url = url.replace('youtube', 'youtube.com/embed');
+      url = url.replace('youtu.be', 'youtube.com/embed');
+      url =
+        url +
+        `?start=${body.selector.value.split('t=')[1].split(',')[0]}&end=${
+          body.selector.value.split('t=')[1].split(',')[1]
+        }`;
+      setImageUrl(
+        `https://img.youtube.com/vi/${
+          url.split('/')[4].split('?')[0]
+        }/maxresdefault.jpg`
+      );
     }
-    fetch(
-      `https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/${
-        url.split('/')[4].split('#')[0]
-      }&width=480&height=360`
-    )
-      .then(response => response.json())
-      .then(data => setImageUrl(data.thumbnail_url));
-  }
+    if (body.id.includes('vimeo')) {
+      url = url.replace('vimeo.com', 'player.vimeo.com/video');
+      if (body.selector && body.selector.value.includes('t=')) {
+        url = url + `#${body.selector.value.split(',')[0]}`;
+      }
+      fetch(
+        `https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/${
+          url.split('/')[4].split('#')[0]
+        }&width=480&height=360`
+      )
+        .then(response => response.json())
+        .then(data => setImageUrl(data.thumbnail_url));
+    }
+  }, []);
 
   return (
     <div style={position}>
