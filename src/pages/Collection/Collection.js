2a6a8e16a64cc05f8064a8873c95d28c3289ccd7
Kelsie Braidwood <33040231+ionab@users.noreply.github.com>
2020-07-01 13:47:33 +0100

    Fix hot reloading issue (#82)

--- src/pages/Collection/Collection.js
+++ src/pages/Collection/Collection.js
@@ -127,6 +127,7 @@ class CollectionPage extends React.Component {
                   <div className="block collection-item w-3 h-min-4">
                     <Link
                       to={
+                        '/' +
                         pageLanguage +
                         '/' +
                         objectLinks[manifest.id || manifest['@id']].replace(
