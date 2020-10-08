46fc1039eb7120f71e401114183c07f0aa3f9fde
glassfish <glassfish@libis-p-aezel-4.lnx.icts.kuleuven.be>
2020-10-08 11:56:45 +0200

    Commit all changes including additions

--- src/components/Grid/Grid.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Grid = (onClick, currentlySelected, items) => (
-  <React.Fragment>
-    {items.map(item => (
-      <div>{item}</div>
-    ))}
-  </React.Fragment>
-);
--- src/gatsby-demo-helper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// TODO: add demo static static query results as a parameter.
-const GatsbyDemoHelper = ({ children, pathPrefix }) => {
-  window.__PATH_PREFIX__ = pathPrefix || '/';
-  return <>{children}</>;
-};
-
-export default GatsbyDemoHelper;
